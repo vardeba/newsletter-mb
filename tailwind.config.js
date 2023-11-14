ed860133b1b6a2b5f149e76ad4eaff42623208c7
Valdecir Teixeira <vardeba@hotmail.com>
2023-11-14 20:08:11 -0300

    feat: newsletter

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// export default {
+//     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
